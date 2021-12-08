52dfb225b77f767d12a4d7f13fbca755ff1464b9
Vyash Bhawan <vyashbhawan@gmail.com>
2021-12-08 17:02:36 +0100

    initial commit

---
+++ src/components/task-card/task-card.component.tsx
@@ -0,0 +1,39 @@
+import "./task-card.styles.scss";
+import ToiletIcon from "../../assets/icons/toilet.svg";
+import ChevronIcon from "../../assets/icons/chevron.svg";
+
+const TaskCard: React.FC = () => {
+  const styleOptions = {
+    main: {
+      backgroundColor: "rgb(4, 52, 141)",
+    },
+    sides: {
+      backgroundColor: "rgb(104, 152, 141)",
+    },
+  };
+  return (
+    <div className="card" style={styleOptions.main}>
+      <div className="card__week">
+        <p>Week 36</p>
+      </div>
+      <div className="card__task-icon">
+        <img src={ToiletIcon} alt="toilet" />
+      </div>
+      <div className="card__header">
+        <h3 className="card__header--title">Toilet boven</h3>
+        <p className="card__header--assigned-to">You</p>
+      </div>
+      <div className="card__description">
+        <div className="card__description--task">Task </div>
+        <div className="card__description--task-info">0/6</div>
+        <div className="card__description--deadline">Deadline</div>
+        <div className="card__description--deadline-info">7 days</div>
+      </div>
+      <div className="card__show">
+        <img src={ChevronIcon} alt="go to task" />
+      </div>
+    </div>
+  );
+};
+
+export default TaskCard;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,44 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import { IonContent, IonPage } from "@ionic/react";
+import axios from "axios";
+import { useEffect } from "react";
+import TaskCard from "../components/task-card/task-card.component";
+import "./Home.scss";
 
 const Home: React.FC = () => {
+  useEffect(() => {
+    axios
+      .request({
+        url: "https://us-central1-bunkies-app.cloudfunctions.net/app/house/9pqaO9JaL852lSgDLm01",
+      })
+      .then((res) => {
+        console.log(res.data.data);
+      });
+  }, []);
+
   return (
     <IonPage>
-      <IonHeader>
+      {/* <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Calendar</IonTitle>
         </IonToolbar>
-      </IonHeader>
+      </IonHeader> */}
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
+        {/* <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
+            <IonTitle size="large">Calendar</IonTitle>
           </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+        </IonHeader> */}
+        <div className="home">
+          <header className="home__date">
+            <h4 className="home__date--label">Today</h4>
+            <p className="home__date--day">06 September 2021</p>
+          </header>
+          <main>
+            <p>This week</p>
+            <TaskCard />
+          </main>
+          <footer>Footer Content — Header.com 2020</footer>
+        </div>
       </IonContent>
     </IonPage>
   );
